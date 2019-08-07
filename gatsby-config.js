77ec2cc73e50be9222f163aa2d58c737e25956b0
Michael Pallister <michael@micp.co.uk>
2019-08-07 16:04:18 +0100

    Reverting

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,13 +35,6 @@ module.exports = {
         path: `${__dirname}/news`
       }
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `content-pages`,
-        path: `${__dirname}/content`
-      }
-    },
     `gatsby-transformer-remark`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
