8829402e3d97e5677238a40adafc4bd499d23913
Michael Pallister <michael@micp.co.uk>
2019-08-21 13:35:01 +0100

    Small colour and visual tweaks

--- src/components/home/news-articles.js
+++ src/components/home/news-articles.js
@@ -50,37 +50,44 @@ export default function NewsArticles() {
 
   return (
     <section className="home-news">
-      <h2 className="home__h2">Latest News</h2>
-      <div class="home-news__articles">
-        <div className="home-news__article">
-          {article.frontmatter.image &&
-            article.frontmatter.image.childImageSharp.fluid && (
+      <div className="container">
+        <h2 className="home__h2">Latest News</h2>
+        <div class="home-news__articles">
+          <div className="home-news__article">
+            {article.frontmatter.image &&
+              article.frontmatter.image.childImageSharp.fluid && (
+                <Link to={article.frontmatter.path}>
+                  <Img
+                    fluid={article.frontmatter.image.childImageSharp.fluid}
+                  />
+                </Link>
+              )}
+            <h3 className="home-news__h3">
               <Link to={article.frontmatter.path}>
-                <Img fluid={article.frontmatter.image.childImageSharp.fluid} />
+                {article.frontmatter.title}
               </Link>
-            )}
-          <h3 className="home-news__h3">
-            <Link to={article.frontmatter.path}>
-              {article.frontmatter.title}
-            </Link>
-          </h3>
-          <time className="home-news__date" dateTime={article.frontmatter.date}>
-            {article.frontmatter.date}
-          </time>
-          <p className="home-news__p">{article.excerpt}</p>
-        </div>
-        <div>
-          {nodes.map(({ frontmatter, excerpt }) => (
-            <div className="home-news__article">
-              <h3 className="home-news__h3">
-                <Link to={frontmatter.path}>{frontmatter.title}</Link>
-              </h3>
-              <time className="home-news__date" dateTime={frontmatter.date}>
-                {frontmatter.date}
-              </time>
-              <p className="home-news__p">{excerpt}</p>
-            </div>
-          ))}
+            </h3>
+            <time
+              className="home-news__date"
+              dateTime={article.frontmatter.date}
+            >
+              {article.frontmatter.date}
+            </time>
+            <p className="home-news__p">{article.excerpt}</p>
+          </div>
+          <div>
+            {nodes.map(({ frontmatter, excerpt }) => (
+              <div className="home-news__article">
+                <h3 className="home-news__h3">
+                  <Link to={frontmatter.path}>{frontmatter.title}</Link>
+                </h3>
+                <time className="home-news__date" dateTime={frontmatter.date}>
+                  {frontmatter.date}
+                </time>
+                <p className="home-news__p">{excerpt}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
